55ccb934f2a21a07d3d83205803dfe4cc27b8a68
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-02-29 22:39:05 +0530

    developing

---
+++ qr-webapp/src/components/QRScannerView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function QRScannerView() {
+  return (
+    <>
+     <div className='flex justify-center items-center w-screen h-screen'>
+        <div className='w-96 h-96 bg-red-500'></div>
+     </div>
+    </>
+  )
+}
+
+export default QRScannerView
